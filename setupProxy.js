4d0009f959146e7d026632ab1c2b4aeeff1a063b
zaineb <122824506+zaineb15@users.noreply.github.com>
2024-03-31 00:55:41 +0100

    Add files via upload

---
+++ setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8000', // Adresse de votre backend Laravel
+      changeOrigin: true,
+    })
+  );
+};
